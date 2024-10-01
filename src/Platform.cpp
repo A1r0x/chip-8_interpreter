0a5614e9e0fbdf10aac35d27e251155fb93e9d95
Alex Ma <31422972+A1r0x@users.noreply.github.com>
2024-10-01 17:31:03 -0400

    Create constructor and destructor functions for Platform class

--- src/Platform.cpp
+++ src/Platform.cpp
@@ -1,3 +1,22 @@
 #include "Platform.hpp"
+#include <cstdint>
 #include <SDL.h>
 #include <SDL_render.h>
+
+Platform::Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight) {
+    // Initializes SDL library with video subsystem to enable graphics
+    SDL_init(SDL_INIT_VIDEO);
+    // Creates a window with the given title, width, and height
+    window = SDL_CreateWindow(title, 0, 0, windowWidth, windowHeight, SDL_WINDOW_SHOWN);
+    // Creates a renderer to handle rendering within the window, using hardware acceleration 
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    // Creates a texture to store pixel data for the display, in RGBA8888 format with streaming access
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_STREAMING, textureWidth, textureHeight);
+}
+
+Platform::~Platform() {
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
--- src/Platform.hpp
+++ src/Platform.hpp
@@ -4,8 +4,11 @@
 
 class Platform {
 public:
+	// Constructor
     Platform(char const* title, int windowWidth, int windowHeight, int textureWidth, int textureHeight);
+	// Destructor
 	~Platform();
+	// Handles drawing the graphics by updating the texture with new pixel data then rendering it to the screen
 	void Update(void const* buffer, int pitch);
 	bool ProcessInput(uint8_t* keys);
 private:

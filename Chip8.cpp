6ab70ebe1716584b6b9f53dffa17b41202be667e
Alex Ma <31422972+A1r0x@users.noreply.github.com>
2024-10-01 15:49:37 -0400

    Implement function that emulates one cycle of the CPU

--- Chip8.cpp
+++ Chip8.cpp
@@ -124,6 +124,27 @@ void Chip8::LoadROM(char const* filename) {
     }
 }
 
+void Chip8::Cycle() {
+    // Fetch
+    opcode = (memory[pc] << 8u) | memory[pc + 1];
+
+    // Increment the PC before we execute anything
+	pc += 2;
+
+    // Decode and execute the opcode
+    ((*this).*(table[(opcode & 0xF000u) >> 12u]))();
+
+    // Decrement the delay timer if it's been set
+    if (delayTimer > 0) {
+        delayTimer--;
+    }
+
+    // Decrement the sound time if it's been set
+    if (soundTimer > 0) {
+        soundTimer--;
+    }
+}
+
 // clear the display
 void Chip8::OP_00E0() {
     memset(video, 0, sizeof(video));
